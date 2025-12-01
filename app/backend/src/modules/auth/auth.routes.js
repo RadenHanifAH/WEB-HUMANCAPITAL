65448ec0519ef85fcb992bd4c8b5711eed18f7f1
rafleeea <rafli.10522118@mahasiswa.unikom.ac.id>
2025-12-01 11:37:55 +0700

    add auth middleware

--- app/backend/src/modules/auth/auth.routes.js
+++ app/backend/src/modules/auth/auth.routes.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("./auth.controller");
+const authMiddleware = require("../../middleware/auth");
+
 
 router.post("/register", authController.register);
 router.post("/login", authController.login);
 router.post("/refresh-token", authController.refreshAccessToken);
 router.post("/logout", authController.logout);
-router.get("/profile", authController.getProfile);
-router.put("/profile/:id", authController.updateProfile)
+router.get("/profile", authMiddleware.protectRoute, authController.getProfile);
+router.put("/profile/:id", authMiddleware.protectRoute, authController.updateProfile)
 
 module.exports = router;
