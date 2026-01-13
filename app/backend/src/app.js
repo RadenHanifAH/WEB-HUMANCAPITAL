38f84a013b5c2d1ed1978785d8027e687095c355
RadenHanifAH <radenhanifabdulhakim67@gmail.com>
2026-01-13 14:56:37 +0700

    feat: add cors

--- app/backend/src/app.js
+++ app/backend/src/app.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 // ✅ 2) cors untuk credentials
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://web-humancapital.vercel.app/",
     credentials: true,
   })
 );
