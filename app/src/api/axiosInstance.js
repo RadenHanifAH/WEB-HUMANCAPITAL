7b77efa2654d64ad48a2db0d5b5612d4d9b937d8
RadenHanifAH <radenhanifabdulhakim67@gmail.com>
2026-01-19 23:17:57 +0700

    update

--- app/src/api/axiosInstance.js
+++ app/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://web-humancapital-production.up.railway.app/api",
+  baseURL: "https://web-humancapital-production.up.railway.app/api",
   withCredentials: true,
 });
 
