7cfb169944fa8dd41e08d09ff62358c441554cf9
unknown <radenhanifabdulhakim67@gmail.com>
2026-08-12 15:34:31 +0700

    lagi

--- app/backend/src/server.js
+++ app/backend/src/server.js
@@ -1,7 +1,15 @@
 // app/backend/src/server.js
 const app = require('./app');
 
-const PORT = process.env.PORT || 4000;
+// Port standar untuk API: 4000
+const PORT = process.env.PORT || 4000; 
+
+// app.listen(PORT, () => {
+//     console.log(`✅ Server lowongan berjalan di http://localhost:${PORT}`);
+//     // PERUBAHAN: Mengganti 'api/positions' menjadi 'api/dashboard/data'
+//     console.log(`   Endpoint untuk Dashboard: http://localhost:${PORT}/api/dashboard/data`); 
+// });
+
 
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`✅ Server berjalan di port ${PORT}`);
